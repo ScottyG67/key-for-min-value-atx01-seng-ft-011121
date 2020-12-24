c6baa52cbeb6397fc5648ce9964a36451772efb9
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-24 20:21:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,15 @@
 
 def key_for_min_value(name_hash)
 key=nil
-name_hash.each
+test=10
+name_hash.each do |keys,value|
+  if value
+
+winner=""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
